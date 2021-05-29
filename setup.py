946c6c55700389ea46e0ce2039a2f7a27def6fa0
nolze <nolze@archlinux.us>
2021-05-30 04:38:05 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ INSTALL_REQUIRES = [
     'colorama<=0.4.1;sys_platform=="win32" and python_version=="3.4"',
     'colorama;sys_platform=="win32" and python_version!="3.4"',
     "pluggy>=0.12,<1.0",
-    'importlib-metadata>=0.12,==0.12;python_version<"3.8"',
+    "zipp==0.5",
+    'importlib-metadata>=0.12,<3.0;python_version<"3.8"',
     "wcwidth",
 ]
 
